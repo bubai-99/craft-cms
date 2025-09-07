30d3ae39ffd8341430229020d595ec6baefc6a5b
Binu <binu@example.com>
2025-09-07 11:53:24 +0000

    frontend setup with vite

---
+++ config/vite.php
@@ -0,0 +1,18 @@
+<?php
+
+use craft\helpers\App;
+
+return [
+    'useDevServer' => App::env('CRAFT_ENVIRONMENT') === 'dev',
+    'manifestPath' => '@webroot/dist/.vite/manifest.json',
+    'devServerPublic' => App::env('PRIMARY_SITE_URL') . ':5173',
+    'serverPublic' => App::env('PRIMARY_SITE_URL') . 'dist/',
+    'errorEntry' => '',
+    'cacheKeySuffix' => '',
+    'devServerInternal' => 'http://localhost:5173',
+    'checkDevServer' => true,
+    'includeReactRefreshShim' => false,
+    'includeModulePreloadShim' => true,
+    'criticalPath' => '@webroot/dist/criticalcss',
+    'criticalSuffix' => '_critical.min.css',
+];
